e043c83624cc1122056388460f61107f4c7d3760
Kim Kao <yikaikao@gmail.com>
2019-09-19 22:50:38 +0800

    modify DomainEvent.

--- sources/coffeeshop/ddd-commons/src/main/java/solid/humank/ddd/commons/baseclasses/DomainEvent.java
+++ sources/coffeeshop/ddd-commons/src/main/java/solid/humank/ddd/commons/baseclasses/DomainEvent.java
@@ -1,8 +1,26 @@
 package solid.humank.ddd.commons.baseclasses;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import solid.humank.ddd.commons.interfaces.IDomainEvent;
 import solid.humank.ddd.commons.interfaces.IEntityId;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
 public abstract class DomainEvent<T extends IEntityId> extends ValueObject<DomainEvent> implements IDomainEvent {
 
+    @Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE)
+    private UUID eventId;
+
+    @Getter(AccessLevel.PUBLIC) @Setter(AccessLevel.PRIVATE)
+    private OffsetDateTime occurredDate;
+    private void setEventId(UUID eventId){
+        this.eventId = eventId;
+
+    }
+
+    abstract DomainEvent raise(IEntityId entityId, Optional<OffsetDateTime> occurredDate);
 }
