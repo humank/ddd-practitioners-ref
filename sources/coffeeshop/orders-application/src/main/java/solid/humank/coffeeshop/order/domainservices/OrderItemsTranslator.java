1ee9a5137afc5600687ab0f406e40276bcb2920e
Kim Kao <yikaikao@gmail.com>
2019-10-15 16:44:05 +0800

    debug for orderitem empty issue in native image.

--- sources/coffeeshop/orders-application/src/main/java/solid/humank/coffeeshop/order/domainservices/OrderItemsTranslator.java
+++ sources/coffeeshop/orders-application/src/main/java/solid/humank/coffeeshop/order/domainservices/OrderItemsTranslator.java
@@ -1,5 +1,7 @@
 package solid.humank.coffeeshop.order.domainservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import solid.humank.coffeeshop.order.datacontracts.results.OrderItemRst;
 import solid.humank.coffeeshop.order.models.OrderItem;
 import solid.humank.ddd.commons.interfaces.ITranslator;
@@ -11,10 +13,15 @@ import java.util.List;
 @Dependent
 public class OrderItemsTranslator implements ITranslator<List<OrderItem>, List<OrderItemRst>> {
 
+    Logger logger = LoggerFactory.getLogger(OrderItemsTranslator.class);
     @Override
     public List<OrderItem> translate(List<OrderItemRst> transRequest) {
         List<OrderItem> orderItemList = new ArrayList<>();
         for (OrderItemRst orderItemRst : transRequest) {
+
+            logger.info("orderItemRst.getProductId(): "+ orderItemRst.getProductId());
+            logger.info("orderItemRst.getQty(): "+ orderItemRst.getQty());
+            logger.info("orderItemRst.getPrice() "+ orderItemRst.getPrice());
             orderItemList.add(
                     new OrderItem(orderItemRst.getProductId(),
                             orderItemRst.getQty(),
