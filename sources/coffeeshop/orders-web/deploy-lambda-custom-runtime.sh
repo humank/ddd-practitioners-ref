da341a1bd89bd423c8237841fa341dcb48759801
humank <yikaikao@gmail.com>
2019-10-21 04:43:00 +0000

    test for custom lambda runtime to accept requests from apig but fail.

--- sources/coffeeshop/orders-web/deploy-lambda-custom-runtime.sh
+++ sources/coffeeshop/orders-web/deploy-lambda-custom-runtime.sh
@@ -6,7 +6,7 @@ BUNDLEDIR=target/bundle
 function bundle() {
 
     #mvn clean package -Pnative  -DskipTests=true -Dnative=true -Dsvm.platform=org.graalvm.nativeimage.impl.InternalPlatform$LINUX_JNI_AMD64
-    mvn clean package -DskipTests=true -Dnative=true -Dnative-image.docker-build=true
+    mvn clean package -DskipTests=true -Dnative=true -Dnative-image.docker-build=true -Dsvm.platform=org.graalvm.nativeimage.impl.InternalPlatform$LINUX_JNI_AMD64
     
     mkdir -p ${BUNDLEDIR}
     cp -r target/wiring-classes/bootstrap ${APPDIR}/*-runner  ${BUNDLEDIR}
@@ -14,8 +14,15 @@ function bundle() {
     #sed -i 's/$RUNNER/$RUNNER -Djava.net.preferIPv4Stack=true -Djava.library.path=\/opt\/graalvm\/jre\/lib\/amd64 -Djavax.net.ssl.trustStore=\/opt\/graalvm\/jre\/lib\/security\/cacerts/g' ${BUNDLEDIR}/bootstrap
     sed -i 's/$RUNNER/$RUNNER -Djava.net.preferIPv4Stack=true -Djava.library.path=\/opt\/graalvm\/jre\/lib\/amd64 -Djavax.net.ssl.trustStore=\/opt\/graalvm\/jre\/lib\/security\/cacerts/g' ${BUNDLEDIR}/bootstrap    
     
-    chmod 755 ${BUNDLEDIR}/bootstrap
-    cd ${BUNDLEDIR} && zip -q function.zip bootstrap *-runner ; cd -
+
+    #chmod 755 ${BUNDLEDIR}/bootstrap
+    # copy to outside and zip for deploying
+    #cd ${BUNDLEDIR} && zip -q function.zip bootstrap *-runner ; cd -
+    
+    cp bootstrap $BUNDLEDIR/
+    chmod 755 $BUNDLEDIR/bootstrap
+    cd $BUNDLEDIR && zip -q function.zip bootstrap *-runner ; cd -
+    
 
 }
 
