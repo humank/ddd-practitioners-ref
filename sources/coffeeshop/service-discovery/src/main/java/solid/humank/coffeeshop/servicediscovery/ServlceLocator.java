972bb452049e1c48747cc12d4bc387dacec827d5
kimkao <yikaikao@gmail.com>
2019-11-01 00:42:57 +0800

    select resteasy client to provide http invocation.

--- sources/coffeeshop/service-discovery/src/main/java/solid/humank/coffeeshop/servicediscovery/ServlceLocator.java
+++ sources/coffeeshop/service-discovery/src/main/java/solid/humank/coffeeshop/servicediscovery/ServlceLocator.java
@@ -4,5 +4,5 @@ public class ServlceLocator {
 
     //TODO: 完成新增配置設定檔，讓預設的配置先看本地端，再去找App Mesh上的 Virtual Service endpoint
 
-    //TODO: 完成提供http client 訪問的封裝, 需要包裝 JAX-RS client
+    //TODO: 完成提供http client 訪問的封裝, 需要包裝 JAX-RS client, 像是 Resteasy client (與quarkus-resteasy相容應該好一些)
 }
