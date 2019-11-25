f75acc9388c335f20a6b38160e042c09caafcdf6
Kim Kao <yikaikao@gmail.com>
2019-11-25 17:44:37 +0800

    add yes pipe.

--- deployment/coffeeshop-cdk/lib/coffee-shop-code-pipeline.ts
+++ deployment/coffeeshop-cdk/lib/coffee-shop-code-pipeline.ts
@@ -81,7 +81,7 @@ export class CoffeeShopCodePipeline extends cdk.Stack {
 
                     install:{
                         commands:[
-                           'sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"',
+                           'yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"',
                             'test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)',
                             'test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)',
                             'test -r ~/.bash_profile && echo "eval \\$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile',
