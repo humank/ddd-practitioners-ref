993384bdbb18d46f69314d8c47afc988eb28a826
kimkao <yikaikao@gmail.com>
2019-12-07 00:54:52 +0800

    modify docker build file for native usage.

---
+++ sources/coffeeshop/orders-web/src/main/docker/build-image.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -f src/main/docker/Dockerfile.multistage -t kim-test/orders-web .
--- sources/coffeeshop/orders-web/src/main/docker/run-orders-web.sh
+++ sources/coffeeshop/orders-web/src/main/docker/run-orders-web.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run -i --rm -p 8080:8080 solid-humank-coffeeshop/orders-web
+# docker run -i --rm -p 8080:8080 solid-humank-coffeeshop/orders-web
+docker run -i --rm -p 8080:8080 kim-test/orders-web
