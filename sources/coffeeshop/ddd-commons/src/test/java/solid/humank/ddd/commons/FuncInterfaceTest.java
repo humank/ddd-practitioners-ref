52ce28e31033bdbe0bbbee58fe29e28f082987f5
YiKai Kao <yikaikao@amazon.com>
2020-12-25 02:35:30 +0800

    fix wrong sample test.

--- sources/coffeeshop/ddd-commons/src/test/java/solid/humank/ddd/commons/FuncInterfaceTest.java
+++ sources/coffeeshop/ddd-commons/src/test/java/solid/humank/ddd/commons/FuncInterfaceTest.java
@@ -2,6 +2,10 @@ package solid.humank.ddd.commons;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 @FunctionalInterface
 interface Converter {
     double convert(double input);
@@ -24,5 +28,10 @@ public class FuncInterfaceTest {
         System.out.println(sut.convert(input -> (input - 32) * 5.0 / 9.0, 98.6));
         // Convert Kilometers to Miles
         System.out.println(sut.convert(input -> input / 1.609344, 8));
+    }
+
+    @Test
+    public void streamAPITest() throws FileNotFoundException {
+        //BufferedReader bufferedReader = new BufferedReader(new FileReader("test.txt"));
     }
 }
